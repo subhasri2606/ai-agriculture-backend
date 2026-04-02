a552ed7a5dd039a43806a83fa3c642c445e93ef0
subhasri2606 <subhasri2621@gmail.com>
2026-04-02 19:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+@app.route("/")
+def home():
+    return "AI Agriculture Backend Running 🚀"
 
 @app.route("/recommend", methods=["POST"])
 def recommend():
